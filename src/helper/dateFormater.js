4ad1b8ff5fa7ee2f3d92585c56505aef73de604e
denis.zhidis <denis_zhidis@mail.ru>
2018-05-18 18:19:56 +0300

    fixed date

--- src/helper/dateFormater.js
+++
@@ -1,6 +0,0 @@
-export const formatDate = date =>
-  `${date.getFullYear()}-${
-    date.getUTCMonth() + 1 < 10
-      ? '0' + (date.getUTCMonth() + 1)
-      : date.getUTCMonth() + 1
-  }-${date.getUTCDate() < 10 ? '0' + date.getUTCDate() : date.getUTCDate()}`;
--- src/redux/modules/app/actions.js
+++ src/redux/modules/app/actions.js
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import ACTION_CONSTANTS from './constants';
 import * as insightsApi from '../../../api/insightsApi';
 import {
@@ -5,7 +6,6 @@ import {
   GET_NEW_INSIGHTS,
 } from '../insights/insightsActionConstants';
 import { mapInsightFromApi } from '../../../helper/apiDataMapper';
-import { formatDate } from '../../../helper/dateFormater';
 import { sortInsightsByCategoryAndOrder } from '../../../helper/apiDataSorter';
 
 export const hidingInsight = key => (dispatch, getState) => {
@@ -35,13 +35,13 @@ export const refreshThunder = (
 
   const insightsResponse = await insightsApi.getInsights(
     thunderkey,
-    formatDate(selectedDate)
+    format(selectedDate, 'YYYY-MM-DD')
   );
 
   const newInsightsResponse = await insightsApi.getNewInsights(
     thunderkey,
-    formatDate(selectedDate),
-    formatDate(new Date())
+    format(selectedDate, 'YYYY-MM-DD'),
+    format(new Date(), 'YYYY-MM-DD')
   );
   const { categories } = getState().categories;
 
